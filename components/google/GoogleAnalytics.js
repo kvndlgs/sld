5052e687e787534b9c5b6e3b1b6ca16b6a490eab
kvndlgs <poissedonn@gmail.com>
2023-10-13 21:06:40 -0400

    some shit1

--- components/google/GoogleAnalytics.js
+++ components/google/GoogleAnalytics.js
@@ -1,25 +1,22 @@
 import Script from "next/script"
 
 
-
 const GoogleAnalytics = () => {
-    return (
-      <>
-        <Script
-          src={`src="https://www.googletagmanager.com/gtag/js?id=G-LN7CCNMJDJ"`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
-         
-           gtag('config', 'G-LN7CCNMJDJ');
+  return (
+    <>
+      <Script async
+        src="https://www.googletagmanager.com/gtag/js?id=G-LN7CCNMJDJ" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+         gtag('config', 'G-LN7CCNMJDJ');
           `}
-        </Script>
-      </>
-    );
-  };
-  
-  export default GoogleAnalytics;
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
