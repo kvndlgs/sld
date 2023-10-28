af582713ad4bae1ee06c7b411a3d5f1ba8251011
kvndlgs <poissedonn@gmail.com>
2023-10-28 12:46:33 -0400

    '234'

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,15 +6,6 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: 'kvndesloges@gmail.com',
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
   }
 }
