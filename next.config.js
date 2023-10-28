9830878681482aae767f740901acbe3c56c81adc
kvndlgs <poissedonn@gmail.com>
2023-10-28 12:40:48 -0400

    '4'

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "rules": {
-        "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
-    }
+    
 }
 
 module.exports = nextConfig
