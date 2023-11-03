f5e7dfb99097e6cc510b3c6dc0c51543dd38ae41
kvndlgs <poissedonn@gmail.com>
2023-11-02 22:50:01 -0400

    '8'

--- app/api/send.ts
+++ app/api/send.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-// import { Mail } from '@/app/components/mail/page'
 import { resend } from '@/app/lib/resend'
 import * as React from 'react';
 
@@ -10,7 +9,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         to: ['info@shieldsignalisation.com'],
         subject: 'Fuck a bitch',
         html: '<body><h1> hey </h1></body>',
-//        react: (<Mail fullName: 'John', email: 'onboarding@resend.dev', role: 'designer', company: 'Acme', phone: '911', message: 'fuck off bitch' />),
       });
   
       res.status(200).json(data);
