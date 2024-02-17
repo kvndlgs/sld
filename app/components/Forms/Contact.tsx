95f010995977a616ff76a71b09fa5aad34bcaf64
kvndlgs <poissedonn@gmail.com>
2024-02-17 23:40:17 +0000

    feat: Add Contact component and form with validation and submission logic

---
+++ app/components/Forms/Contact.tsx
@@ -0,0 +1,138 @@
+"use client";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+import { sendEmail } from "@/utils/sendEmail";
+import * as Icon from "react-icons/lu";
+
+export type FormData = {
+  name: string;
+  email: string;
+  phone: string;
+  company: string;
+  role: string;
+  message: string;
+};
+
+const Contact: FC = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+  const [isSent, setIsSent] = useState(false);
+
+  function onSubmit(data: FormData) {
+    sendEmail(data);
+    setIsSent(true);
+  }
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full px-8 md:py-20 py-16"
+    >
+      <div className="flex flex-col items-start justify-between w-full md:w-3/4 mx-auto">
+        <label
+          htmlFor="fullname"
+          className="text-darky-400 font-medium ml-2 pb-2"
+        >
+          Nom
+        </label>
+        <input
+          type="text"
+          name="fullname"
+          placeholder="Entrer votre nom"
+          className="w-full py-3 px-2 text-darky-600 placeholder-darky-400  font-medium bg-darky-100 rounded-md border-2 border-transparent focus:border-2 focus:border-primary-300 outline-none transition-all ease-in-out"
+          {...register("name", { required: true })}
+        />
+      </div>
+
+      <div className="w-full flex flex-col items-start justify-around pt-2 pb-3 pr-2 md:w-3/4 mx-auto">
+        <label
+          htmlFor="email"
+          className="text-darky-400 font-medium text-base ml-2 pb-2"
+        >
+          Courriel
+        </label>
+        <input
+          type="string"
+          name="email"
+          placeholder="Entrer votre courriel"
+          className="w-full py-3 px-2 text-darky-600 placeholder-darky-600  font-medium bg-darky-100 rounded-md border-2 border-transparent focus:border-2 focus:border-primary-300 outline-none transition-all ease-in-out"
+          {...register("email", { required: true })}
+        />
+      </div>
+      <div className="w-full flex flex-col items-start justify-center pt-2 pb-3 pr-2 md:pl-2 md:w-3/4 mx-auto">
+        <label
+          htmlFor="phone"
+          className="text-darky-400 font-medium text-base ml-2 pb-2"
+          {...register("phone", { required: true })}
+        >
+          Téléphone
+        </label>
+        <input
+          type="number"
+          name="phone"
+          placeholder="Entrer votre téléphone"
+          className="w-full py-3 px-2 text-darky-600 placeholder-darky-600  font-medium bg-darky-100 rounded-md border-2 border-transparent focus:border-2 focus:border-primary-300 outline-none transition-all ease-in-out"
+        />
+      </div>
+      <div className="w-full flex md:flex-row flex-col md:justify-between justify-center items-center md:items-start md:w-3/4 mx-auto">
+        <div className="w-full flex flex-col items-start justify-around pt-2 pb-3 pr-2">
+          <label
+            htmlFor="company"
+            className="text-darky-400 font-medium text-base ml-2 pb-2"
+            {...register("company", { required: true })}
+          >
+            Entreprise
+          </label>
+          <input
+            type="text"
+            name="company"
+            placeholder="Entrer le nom de l'entreprise"
+            className="w-full py-3 px-2 text-darky-600 placeholder-darky-600  font-medium bg-darky-100 rounded-md border-2 border-transparent focus:border-2 focus:border-primary-300 outline-none transition-all ease-in-out"
+          />
+        </div>
+        <div className="w-full flex flex-col items-start justify-around  pt-2 pb-3 pl-2">
+          <label
+            htmlFor="role"
+            className="text-darky-400 font-medium text-base ml-2 pb-2"
+          >
+            Role
+          </label>
+          <input
+            type="text"
+            name="role"
+            placeholder="Votre role dans l'entreprise"
+            className="w-full py-3 px-2 text-darky-600 placeholder-darky-600  font-medium bg-darky-100 rounded-md border-2 border-transparent focus:border-2 focus:border-primary-300 outline-none transition-all ease-in-out"
+            {...register("role", { required: true })}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-start justify-around pt-2 pb-3 w-full md:w-3/4 mx-auto">
+        <label
+          htmlFor="message"
+          className="text-darky-400 font-medium text-base ml-2 pb-2"
+        >
+          Message
+        </label>
+        <textarea
+          name="message"
+          placeholder="Entrez les détails"
+          className="w-full pt-2 pb-12 px-2 text-darky-600 placeholder-darky-600  font-medium bg-darky-100 rounded-md border-2 border-transparent focus:border-2 focus:border-primary-300 outline-none transition-all ease-in-out"
+          {...register("message", { required: true })}
+        ></textarea>
+      </div>
+
+      <div className="flex flex-col items-start justify-around pt-2 pb-2 w-full md:w-3/4 mx-auto">
+        <Button
+          size="md"
+          icon="true"
+          label={isLoading ? "En cours..." : "Envoyer"}
+          type="submit"
+        >
+          {isLoading ? (
+            <Icon.LuLoader size="18" />
+          ) : (
+            <Icon.LuArrowRight size="18" />
+          )}
+        </Button>
+      </div>
+    </form>
+  );
+};
